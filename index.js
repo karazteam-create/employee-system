ce0d12df7b9c59e1577e967033de644ae90428cc
karazteam-create <karazteam@gmail.com>
2026-01-14 03:02:57 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("fs");
 
-// ضبط مجلد الـ Views بدقة
+// ضبط مجلد الـ Views
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+// تحميل بيانات الشركات من ملف JSON
+const companies = JSON.parse(fs.readFileSync(path.join(__dirname, "data/companies.json")));
+
 // صفحة رئيسية
 app.get("/", (req, res) => {
-  res.render("index", { message: "Employee System is working!" });
+  res.render("index", { companies });
 });
 
-// منفذ السيرفر
 const port = process.env.PORT || 10000;
 app.listen(port, () => {
   console.log("Server running on port " + port);
